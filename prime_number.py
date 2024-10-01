bbecc8435d08afb9e5b570639925a174497b8656
fuuuua <f.tanaka.24.8.1568@4nonome.com>
2024-10-01 23:58:49 +0900

    2024/09/28 制御構文

--- prime_number.py
+++ prime_number.py
@@ -1,23 +1,22 @@
 a = 61
 b = 10
 
-# TODO
 if a <= 1:
     print(False)
 else:
+    is_prime_a = True
     for i in range(2, int(a**0.5)+1):
         if a % i == 0:
-            print(False)
-        break
-    else:
-        print(True)
+            is_prime_a = False
+            break
+print(is_prime_a)
 
 if b <= 1:
     print(False)
 else:
+    is_prime_b = True
     for i in range(2, int(b**0.5)+1):
         if b % i == 0:
-            print(False)
-        break
-    else:
-        print(True)
+            is_prime_b = False
+            break
+print(is_prime_b)
